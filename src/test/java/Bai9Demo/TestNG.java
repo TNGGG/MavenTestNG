10d7d7bf5db0c189d6015935f65cbcc0934213dd
TNGGG <linh3712@gmail.com>
2024-10-23 20:44:01 +0700

    thwr

---
+++ src/test/java/Bai9Demo/TestNG.java
@@ -0,0 +1,14 @@
+package Bai9Demo;
+
+import org.testng.annotations.Test;
+
+public class TestNG {
+    @Test
+    public void testLoginCRM(){
+        System.out.println("Nme");
+    }
+    @Test
+    public void TC2(){
+        System.out.println("TC2");
+    }
+}
