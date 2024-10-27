5c4bf3fb961140a8abaa49c6cc921c4dd798ca6d
TNGGG <linh3712@gmail.com>
2024-10-27 16:30:56 +0700

    thwr

---
+++ src/test/java/Bai9Demo/TestNG2.java
@@ -0,0 +1,10 @@
+package Bai9Demo;
+
+import org.testng.annotations.Test;
+
+public class TestNG2 {
+    @Test
+    public void Test(){
+        System.out.println(" - ");
+    }
+}
---
+++ src/test/java/Bai9Demo/TestNG3.java
@@ -0,0 +1,10 @@
+package Bai9Demo;
+
+import org.testng.annotations.Test;
+
+public class TestNG3 {
+    @Test
+    public void testManageProject(){
+        System.out.println("Add new project CRM");
+    }
+}
